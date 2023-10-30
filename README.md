8250b9f563d1ee3e492d1a94c230513372c888b5
Borja Álvarez <borjaalvarez_18@hotmail.com>
2023-10-30 18:44:22 +0000

    03-Configure-typescript

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# Node
+# MyBack
 
-Teminar de adaptar TS, hacer que funcione comando: yarn tsc ./server.ts
+## 03 - Configure typescript
+- Create tsconfig with command `tsc --init`
+- Adapt tsconfig.json
+- Install types for node.js with command `npm i --save-dev @types/node` 
+- Install types for express.js with command `npm i --save-dev @types/express`
+- Install ts-node to run the app with command `yarn add -D ts-node`  
+- Install ts-node-dev (nodemon + ts-node) to run the app with command `yarn add -D ts-node-dev`  
+- Add scripts to package.json 
+- Update .vscode/launch.json to run the app pressing F5
 
-instalar Typescript:    `yarn add typescript --dev`  
-generar tsconfig:   `tsc --init`
+## 02 - Add typescript support
+- Add typescript dev decency with command `yarn add typescript --dev`
+
+## 01 - Initial Commit
+- Install Express app on container
--- dist/server.js
+++ dist/server.js
@@ -2,21 +2,19 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
 'use strict';
-
-import express from "express";
-
-
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 // Constants
 const PORT = 3000;
 const HOST = '0.0.0.0';
-
 // App
-const app = express();
+const app = (0, express_1.default)();
 app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
+    res.send('Hello remote world 2!\n');
 });
-
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
